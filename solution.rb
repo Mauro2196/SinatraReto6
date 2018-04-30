2ebf37d4514a90a729b8939337ccd0fca8ab3fbb
Mauricio Rodríguez <mauriciorodriguez@ZCOBTMH100167-MauricioRodriguez.local>
2018-04-29 20:12:22 -0500

    Added Reto6

---
+++ solution.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+set :session_secret, 'makers'
+enable :sessions
+
+get '/' do
+  @conteo = session[:conteo]
+  erb:index
+
+end
+
+post '/' do
+  erb:contar
+  redirect '/'
+end
+
+
+get '/reset' do
+  session.clear
+  redirect '/'
+end
